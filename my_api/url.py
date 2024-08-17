edae8ee6c7c9eb1c05013d1f3812f4970f43a5bf
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-08-16 23:11:42 -0430

    uploading all files

---
+++ my_api/url.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+# import routers
+from rest_framework import routers, serializers, viewsets
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'userslist', views.MessageListView)
+router.register(r'userscreate', views.MessageCreateView)
+ 
+
+urlpatterns = [
+	path('', include(router.urls)),
+	#path('messages/', views.MessageListView.as_view(), name='message-list'),
+	#path('messages/create/', views.MessageCreateView.as_view(), name='message-create'),
+	path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
